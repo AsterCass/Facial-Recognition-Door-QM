9b7f2e38c1efcbb2ebcae4b9223ed922a4569c85
AsterCasc <astercass@qq.com>
2025-09-26 15:32:50 +0800

    test:

--- client/biz/src/camera/camera_rk.cpp
+++ client/biz/src/camera/camera_rk.cpp
@@ -51,6 +51,33 @@ void rotate90_nv12(const unsigned char *src, unsigned char *dst, int width, int 
     }
 }
 
+void rotate270_nv12(const unsigned char* src, unsigned char* dst, int width, int height) {
+    int wh = width * height;
+    const unsigned char* srcY = src;
+    const unsigned char* srcUV = src + wh;
+
+    unsigned char* dstY = dst;
+    unsigned char* dstUV = dst + wh;
+
+    // Y 平面旋转 270°
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            dstY[(width - x - 1) * height + y] = srcY[y * width + x];
+        }
+    }
+
+    // UV 平面旋转 270°，步长是2
+    for (int y = 0; y < height / 2; y++) {
+        for (int x = 0; x < width; x += 2) {
+            int src_index = y * width + x;
+            int dst_index = (width - x - 2) / 2 * height * 2 + y * 2;
+            dstUV[dst_index] = srcUV[src_index];         // U
+            dstUV[dst_index + 1] = srcUV[src_index + 1]; // V
+        }
+    }
+}
+
+
 
 void faceRecognitionPreFun(void *irFrame, void *rgaFrame, int width, int height) {
     // 这里加锁防止普通和红外摄像头前后脚进入，导致触发两次 faceRecognition
@@ -65,7 +92,7 @@ void faceRecognitionPreFun(void *irFrame, void *rgaFrame, int width, int height)
 
     if (irFrame && MAX_OUTPUT_FRAME_SIZE >= calSize) {
         g_curIrData.size = calSize;
-        rotate90_nv12(static_cast<unsigned char *>(irFrame), g_curIrData.data, width, height);
+        rotate270_nv12(static_cast<unsigned char *>(irFrame), g_curIrData.data, width, height);
         g_curIrData.height = width;
         g_curIrData.width = height;
     }
@@ -75,9 +102,9 @@ void faceRecognitionPreFun(void *irFrame, void *rgaFrame, int width, int height)
 
     if (rgaFrame && MAX_OUTPUT_FRAME_SIZE >= calSize) {
         g_curRgbData.size = calSize;
-        g_curRgbData.height = height;
-        g_curRgbData.width = width;
-        memcpy(g_curRgbData.data, rgaFrame, calSize);
+        rotate90_nv12(static_cast<unsigned char *>(rgaFrame), g_curRgbData.data, width, height);
+        g_curRgbData.height = width;
+        g_curRgbData.width = height;
     }
 
     logPrintln("Free rga and clone",
